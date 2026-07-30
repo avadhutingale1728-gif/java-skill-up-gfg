01fcd859b26f9298bd6e64258a2ff2384df813b2
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-07-30 19:58:19 +0530

    Add Typecasting problem of geeks

---
+++ 01_Java_Basics/Typecast_and_double_it.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        // TypeCast to int double it and print
+        int ab = Integer.parseInt(num);
+        int multi = ab * 2;
+        System.out.print(multi);
+    }
+}
