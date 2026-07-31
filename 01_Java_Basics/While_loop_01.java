5ba84a4edc8989249f0a9bc8df3b177f2611a4f5
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-07-31 19:07:37 +0530

    Add While loop problem

---
+++ 01_Java_Basics/While_loop_01.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        
+        //for(int i= x; i>=0; i--){
+        //  System.out.print(i + " ");
+        while(x>=0){
+            System.out.print(x + " ");
+            x--;
+        }    
+            
+        //}
+    }
+}
