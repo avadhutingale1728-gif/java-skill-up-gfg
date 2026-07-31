f06077eacae4c95cd48beeebebd2bab052ee3b81
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-07-31 19:13:23 +0530

    Add Print Square using *

---
+++ 01_Java_Basics/Print_square.java
@@ -0,0 +1,29 @@
+//draw square pattern using *
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=1; i<= n; i++){
+            for(int j=1; j<=n; j++){
+                
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("* ");
+                    
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+//output if n=4
+// * * * *
+// *     *
+// *     *
+// * * * *
