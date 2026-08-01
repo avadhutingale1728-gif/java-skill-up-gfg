8a69fbe2d1d2bf64a057b920d3bd043a3b6a1bef
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-01 23:01:32 +0530

    Add Function with Arguments in java

---
+++ 02_Method_Arrays_and_strings/Function_with_Arguments.java
@@ -0,0 +1,13 @@
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should return a+b
+class Solution {
+    // code here
+    static int argumentFunction(int a, int b){
+        return a+b;
+    }
+    
+    public static void main(String[] args){
+        Solution.argumentFunction(2,3);
+    }
+}
