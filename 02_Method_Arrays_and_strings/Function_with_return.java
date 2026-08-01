73c13d987687a6e22219ffcb0d02c357e9249a2c
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-01 22:58:44 +0530

    Add Function with return Solution of gfg

---
+++ 02_Method_Arrays_and_strings/Function_with_return.java
@@ -0,0 +1,16 @@
+//Given an integer n, you need to use the return keyword
+//to return the twice the value of n
+
+class Solution {
+    int returnValueFunction(int n) {
+        // code here
+            int multi = n*2;
+            return multi;
+    }
+
+            
+       public static void main(String[] args){
+           Solution obj = new Solution();
+           obj.returnValueFunction(5);
+       }
+}
