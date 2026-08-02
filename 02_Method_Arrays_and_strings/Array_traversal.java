6ffaf92a24bbcddf6c0c223fe037522769c5706b
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-02 23:01:16 +0530

    Add Array traversal

---
+++ 02_Method_Arrays_and_strings/Array_traversal.java
@@ -0,0 +1,9 @@
+//just traverse and print the number
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
