5065f352bc1b7bdacd36ba2792890538767e4ab1
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-02 23:04:57 +0530

    Add for loop

---
+++ 02_Method_Arrays_and_strings/For_each_loop.java
@@ -0,0 +1,11 @@
+//given an array of string 
+class Solution {
+    public void printArray(String[] arr) {
+        // Code here
+        
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+            
+        }
+    }
+}
