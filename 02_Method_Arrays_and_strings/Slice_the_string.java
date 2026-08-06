bdd20f75e39a042c0ef1e608fa9e1b691afc763b
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-06 22:54:41 +0530

    Add Slice the string

---
+++ 02_Method_Arrays_and_strings/Slice_the_string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String sliceString(String s) {
+        // code here
+        
+        //int n = s.length() ;
+        return s.substring(1, s.length()-1);
+        
+    }
+}
