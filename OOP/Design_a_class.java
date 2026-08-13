171b90ff48d78a5b67786db336c3f51723dd55b3
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-13 14:25:19 +0530

    Add design a class in java

---
+++ OOP/Design_a_class.java
@@ -0,0 +1,13 @@
+// Design a class as described in the question
+// code here
+class MyClass{
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
+class main{
+    public static void main(String[] args){
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
