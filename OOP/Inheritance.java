44ccd0a63f96d6d23fac624fe5967ec75a8df46a
Avadhut_Ingale <avadhutingale1728@gmail.com>
2026-08-21 00:17:46 +0530

    Add OOP Learned concept and code

---
+++ OOP/Inheritance.java
@@ -0,0 +1,19 @@
+//parent class
+class Animal{
+    void sound(){
+        System.out.println("Animal sound");
+    }
+}
+
+//child class
+class cat extend Animal{
+    void sound(){
+        System.out.println("cat meows");
+    }
+}
+
+class dog extend Animal{
+    void sound(){
+        System
+    }
+}
